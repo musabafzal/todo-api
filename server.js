c5211692116c6f3630ef95a55382976f38fd5c8e
Musab Afzal <musabafzal@Musabs-MacBook-Pro.local>
2018-07-12 00:06:32 +0500

    added get testing

--- server.js
+++ server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
   })
 })
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }, (err) => {
+      res.status(400).send(err)
+  })
+})
+
 app.listen(8080, () => {
   console.log('Started on port', port)
 })
--- server.test.js
+++ server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest')
 const {app} = require('./server')
 const {Todo} = require('./models/todo')
 
+const todos = [{
+  text: 'First'
+}, {
+  text: 'Second'
+}, {
+  text: 'Third'
+}]
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => {
     done()
   })
 })
@@ -24,7 +34,7 @@ describe('POST /todos', () => {
         if(err){
           return done(err)
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1)
           expect(todos[0].text).toBe(text)
           done()
@@ -47,11 +57,23 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(3)
           done()
         }).catch((err) => {
           done(err)
         })
       })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(3)
+      })
+      .end(done)
   })
 })
