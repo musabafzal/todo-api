70ef9bca11626947bd6b1f1e31f9620c8d1411f4
Musab Afzal <musabafzal@Musabs-MacBook-Pro.local>
2018-07-12 23:01:14 +0500

    very

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
-var _ = require('lodash')
+const _ = require('lodash')
+const bcrypt = require('bcryptjs')
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -34,6 +35,21 @@ var UserSchema = new mongoose.Schema({
   }]
 })
 
+UserSchema.pre('save', function(next) {
+  var user = this
+  
+  if(user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        next()
+      })
+    })
+  } else{
+    next()
+  }
+})
+
 UserSchema.methods.toJSON = function() {
   var user = this
   var userObject = user.toObject()
@@ -50,6 +66,47 @@ UserSchema.methods.generateAuthToken = function() {
 
   return user.save().then(() => {
     return token
+  })
+}
+
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject()
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, succ) => {
+        if(succ){
+          resolve(user)
+        } else {
+          reject(err)
+        }
+      })
+    })
+
+  })
+
+
+}
+
+UserSchema.statics.findByToken = function(token) {
+  var User = this
+  var decoded
+
+  try {
+    decoded = jwt.verify(token, 'abc123')
+  } catch(e) {
+    return new Promise((resolve, reject) => {
+      reject()
+    })
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   })
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var _ = require('lodash')
+var bcrypt = require('bcryptjs')
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user') 
 var {ObjectID} = require('mongodb')
+var {authenticate} = require('./middleware/authenticate')
 
 const port = process.env.PORT || 8080
 
@@ -60,6 +62,22 @@ app.post('/users', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err)
   })
+})
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.set('x-auth', token).send(user)
+    })
+  }).catch((err) => {
+    res.status(401).send()
+  })
+})
+
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
